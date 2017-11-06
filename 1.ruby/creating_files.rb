778b5e63bb6e0c2a274d98dff4b4ade3aa2d02d8
Dahye Choi <dahyee89@gmail.com>
2017-11-06 06:47:11 +0000

    creating files

--- 1.ruby/creating_files.rb
+++ 1.ruby/creating_files.rb
@@ -8,13 +8,17 @@ Dir.chdir("src")
 # 1.txt ~ 30.txt
 # 1.txt => "1번째 내용" 
 
-#File.open("out.txt", "w") do |f|     
-#  f.write(data_you_want_to_write)   
-#end
+#강사님꺼
+30.times do |i|
+  i += 1
+  File.open(i.to_s + ".txt", "w") do |f|     
+      f.write(i.to_s + "번째 내용")
+  end
+end
 
-
+# 내꺼
 for filename in (1..30)
-    file = File.open(filename.to_s + ".txt", "w")
+    file = File.open(filename.to_s + ".txt", "w") #파일을 읽고 쓰고 지우고 등등 할떄 모두 File.open
     file.puts(filename.to_s + "번째 내용")
     file.close
 end
